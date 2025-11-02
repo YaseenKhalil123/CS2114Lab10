d41c60f29f6964ceacb9de6cc94b51f2ca9db8cc
YaseenKhalil123 <yaskhalil2006@vt.edu>
2025-11-02 12:23:29 -0500

    t/test-coverage-update

--- DLListTest.java
+++ DLListTest.java
@@ -380,5 +380,17 @@ public class DLListTest extends TestCase {
         }
         assertNotNull(ex);
     }
+    /**
+    * Problem coverage test for post lab
+    */
+       public void testSize2() {
+        list.add("1");
+        list.add("10");
+        list.add("100");
+        assertEquals(3,list.size());
+        list.clear();
+        assertEquals(0,list.size());
+    }
 
 }
+
